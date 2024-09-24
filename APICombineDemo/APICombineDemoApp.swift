0eee49050da15400ada53b41ba28200c6ed3be50
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-23 21:19:51 -0600

    Initial Commit

---
+++ APICombineDemo/APICombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICombineDemoApp.swift
+//  APICombineDemo
+//
+//  Created by Marco Alonso on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct APICombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
