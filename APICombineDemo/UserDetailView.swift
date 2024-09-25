f63870acdda28418e7c7521d20d3314e5cf0569a
Marco Alonso <marcoalonsorota@hotmail.com>
2024-09-24 21:02:28 -0600

    Showing List of users

---
+++ APICombineDemo/UserDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  UserDetailView.swift
+//  APICombineDemo
+//
+//  Created by Marco Alonso on 24/09/24.
+//
+
+import SwiftUI
+
+struct UserDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserDetailView()
+}
---
+++ APICombineDemo/UserDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserDetailViewModel.swift
+//  APICombineDemo
+//
+//  Created by Marco Alonso on 24/09/24.
+//
+
+import Foundation
